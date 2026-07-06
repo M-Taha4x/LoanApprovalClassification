e7a2ed69a4b3982f5b8c3dae66f2334a03762ac9
M-Taha4x <muhammadtaha6002@gmail.com>
2026-07-07 01:21:02 +0500

    Modified pred.ct.py and train.py

---
+++ src/predict.py
@@ -0,0 +1,59 @@
+import pandas as pd
+import numpy as np
+import joblib
+model=joblib.load(r"D:\ML_Projects\LoanStatusApproval\model\randomforest.pkl")
+scaler=joblib.load(r"D:\ML_Projects\LoanStatusApproval\model\scaler.pkl")
+sample=pd.DataFrame(
+    {
+        'person_age':[20], 
+        'person_gender':[1], 
+        'person_income':[23000], 
+        'person_emp_exp':[1], 
+        'loan_amnt':[39000], 
+        'loan_int_rate':[6], 
+        'loan_percent_income':[0.23], 
+        'cb_person_cred_hist_length':[3], 
+        'credit_score':[710], 
+        'previous_loan_defaults_on_file':[0], 
+        'loan_intent_DEBTCONSOLIDATION':[1], 
+        'loan_intent_EDUCATION':[0], 
+        'loan_intent_HOMEIMPROVEMENT':[0], 
+        'loan_intent_MEDICAL':[0], 
+        'loan_intent_PERSONAL':[0],
+        'loan_intent_VENTURE':[0], 
+        'person_home_ownership_MORTGAGE':[1], 
+        'person_home_ownership_OTHER':[0],
+        'person_home_ownership_OWN':[0], 
+        'person_home_ownership_RENT':[0], 
+        'person_education_Associate':[0], 
+        'person_education_Bachelor':[0], 
+        'person_education_Doctorate':[0], 
+        'person_education_High School':[0], 
+        'person_education_Master':[1]
+    }
+)
+#numerical columns
+num_col = [
+    "person_age",
+    "person_income",
+    "person_emp_exp",
+    "loan_amnt",
+    "loan_int_rate",
+    "loan_percent_income",
+    "cb_person_cred_hist_length",
+    "credit_score"
+]
+#print(scaler.feature_names_in_)
+sample["person_income"] = np.log1p(sample["person_income"])
+sample[num_col]=scaler.transform(sample[num_col])
+prediction=model.predict(sample)
+if prediction[0]==1:
+    print("Loan Approved")
+else:
+    print("Loan Rejected")
+probability = model.predict_proba(sample)
+
+probability = model.predict_proba(sample)
+
+print(f"Probability of Rejection : {probability[0][0]*100:.2f}%")
+print(f"Probability of Approval  : {probability[0][1]*100:.2f}%")
--- src/train.py
+++ src/train.py
@@ -4,7 +4,16 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 import joblib
 df=pd.read_csv("D:\ML_Projects\LoanStatusApproval\data\loan_data.csv")
-num_col=df.select_dtypes(include=['int64','float64']).columns
+num_col = [
+    "person_age",
+    "person_income",
+    "person_emp_exp",
+    "loan_amnt",
+    "loan_int_rate",
+    "loan_percent_income",
+    "cb_person_cred_hist_length",
+    "credit_score"
+]
 cat_col=df.select_dtypes(include=['object','category']).columns
 df['person_income']=np.log1p(df['person_income'])
 from sklearn.preprocessing import LabelEncoder
@@ -30,7 +39,7 @@ x=df.drop('loan_status',axis=1)
 y=df['loan_status']
 from sklearn.model_selection import train_test_split
 X_train,X_test,Y_train,Y_test=train_test_split(x,y,test_size=0.25,random_state=42,stratify=y)
-num_col=x.select_dtypes(include=['int64','float64']).columns
+#num_col=x.select_dtypes(include=['int64','float64']).columns
 from sklearn.preprocessing import StandardScaler
 scaler=StandardScaler()
 X_train[num_col]=scaler.fit_transform(X_train[num_col])
@@ -39,7 +48,8 @@ from sklearn.ensemble import RandomForestClassifier
 rf=RandomForestClassifier(n_estimators=100,random_state=42)
 rf.fit(X_train,Y_train)
 pred_rf=rf.predict(X_test)
-joblib.dump(scaler,'scaler.pkl')
-joblib.dump(rf,'randomforest.pkl')
+joblib.dump(scaler,"D:\ML_Projects\LoanStatusApproval\model\scaler.pkl")
+joblib.dump(rf,r"D:\ML_Projects\LoanStatusApproval\model\randomforest.pkl")
 print("Model Saved Succesfully")
-print(x.columns.to_list())
+#print(x.columns.to_list())
+print(scaler.feature_names_in_)
